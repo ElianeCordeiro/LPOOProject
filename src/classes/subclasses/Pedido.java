078d83d14b5c392701d8e860c55167e9b4321ba7
yJPBO <yjpbo04@gmail.com>
2023-06-19 20:58:04 -0300

    classe pedido criada, exception pagamentonaoconfirmado criada

---
+++ src/classes/subclasses/Pedido.java
@@ -0,0 +1,27 @@
+package classes.subclasses;
+
+import java.util.ArrayList;
+
+import classes.abstrata.Produto;
+import classes.exceptions.PagamentoNaoConfirmadoException;
+
+public class Pedido {
+	private int data;
+	private int id;
+	private Setor setor;
+	private Cliente cliente;
+	private ArrayList<Produto> listaProdutos;
+	private boolean pago;
+	
+	public Pedido(int data, int id, Setor setor, Cliente cliente, ArrayList<Produto> listaProdutos) {
+		this.data = data;
+		this.id = id;
+		this.setor = setor;
+		this.cliente = cliente;
+		this.listaProdutos = listaProdutos;
+	}
+	
+	public void confirmarPagamento(boolean pagamento) throws PagamentoNaoConfirmadoException {
+		this.pago = pagamento;
+	}
+}
