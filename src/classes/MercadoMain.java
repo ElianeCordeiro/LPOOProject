29a35bf40130f2878d2e2d9bc91fd2e341574d78
yJPBO <yjpbo04@gmail.com>
2023-06-21 13:46:36 -0300

    main

--- src/classes/MercadoMain.java
+++ src/classes/MercadoMain.java
@@ -1,10 +1,16 @@
 package classes;
 
-import classes.abstrata.Pessoa;
+import java.util.ArrayList;
+
+import classes.subclasses.Fornecedor;
+import classes.subclasses.Funcionario;
+import classes.subclasses.Pedido;
 
 public class MercadoMain {
 	
 	public static void main(String[] args) {
-		
+		ArrayList<Fornecedor> fornecedores = new ArrayList<>();
+		ArrayList<Pedido> pedidos = new ArrayList<>();
+		ArrayList<Funcionario> funcionarios = new ArrayList<>();
 	}
 }
